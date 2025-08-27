700b58c764c748a436952aebc4768aaed23c1965
techenrikao <techenrikao@outlook.com.br>
2025-08-27 08:50:36 -0300

    Ajusta altura e largura do mapa

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -156,9 +156,7 @@ const ContactSection = () => {
             <Card className="shadow-soft">
               <CardContent className="p-0">
                 <div className="aspect-[4/3] bg-muted rounded-lg flex items-center justify-center">
-                  <div className="text-center">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d697.9489077677473!2d-35.97424934856341!3d-8.283358086634685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a98b3a837b1241%3A0x1245c42122349eb1!2sStudio%20Horya!5e0!3m2!1spt-BR!2sbr!4v1755740010241!5m2!1spt-BR!2sbr" width="658" height="500" loading="lazy"></iframe>
-                  </div>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d697.9489077677473!2d-35.97424934856341!3d-8.283358086634685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a98b3a837b1241%3A0x1245c42122349eb1!2sStudio%20Horya!5e0!3m2!1spt-BR!2sbr!4v1755740010241!5m2!1spt-BR!2sbr" width="100%" height="100%" loading="lazy"></iframe>
                 </div>
               </CardContent>
             </Card>
